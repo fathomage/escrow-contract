5c4e3feff26c60febd1de7ea3dd972f24ca20775
Travis Carlson <travis@fathomage.com>
2024-06-29 13:04:09 -0400

    Initial commit

--- Escrow.sol
+++ Escrow.sol
@@ -1,1 +1,143 @@
-/Users/travis/scaffold-eth-2/packages/hardhat/contracts/Escrow.sol
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+// Useful for debugging. Remove when deploying to a live network.
+import "hardhat/console.sol";
+
+contract Escrow {
+
+	enum Status {
+		DEFAULT, PURCHASED, SHIPPED, RECEIVED, CANCELED
+	}
+
+	struct Transaction {
+		uint id;
+		address buyer;
+		address seller;
+		string item;
+		uint price;
+		Status status;
+	}
+
+	// When the seller sees this event, they know they need to ship the item
+	event ItemPurchased(uint txId, address buyer, address seller, string item, uint price);
+
+	// The sale was completed successfully (item shipped and received by the buyer, payment released to the seller)
+	event SaleCompleted(uint txId, address buyer, address seller, string item, uint price);
+
+	// Buy/sell transactions, mapped by Transaction.id
+	mapping(uint => Transaction) private transactions;
+
+	// Statistics
+	uint private activeTransactions = 0;
+	uint private completedTransactions = 0;
+
+	// Transaction ID generator
+	uint private lastTransactionId = 0;
+
+	// Contract developer
+	address private immutable developer;
+
+	// Called by the buyer
+	// TODO Look up item data from an inventory in the buyer's webapp
+	function purchaseItem(address seller, string memory item) public payable {
+		console.log("Request to purchase item '%s': buyer = %s", item, msg.sender);
+		Transaction memory newTransaction = beginTransaction(msg.sender, seller, item, msg.value);
+		// TODO Watch for this event in the seller's webapp
+		// https://docs.scaffoldeth.io/hooks/useScaffoldWatchContractEvent
+		emit ItemPurchased(newTransaction.id, newTransaction.buyer, newTransaction.seller, newTransaction.item, newTransaction.price);
+	}
+
+	// Called by the seller
+	function shipItem(uint txId) public {
+		require(msg.sender == transactions[txId].seller, "Not the seller");
+		console.log("Item shipped: '%s'", transactions[txId].item);
+		transactions[txId].status = Status.SHIPPED;
+	}
+
+	// Called by the buyer
+	function itemReceived(uint txId) public {
+		require(msg.sender == transactions[txId].buyer, "Not the buyer");
+		console.log("Item received: '%s'", transactions[txId].item);
+		completeTransaction(txId, Status.RECEIVED);
+
+		Transaction memory transaction = transactions[txId];
+		(bool success, ) = transaction.seller.call{ value: transaction.price }("");
+		require(success, "Failed to send Ether");
+		console.log("Payment released to seller %s", transaction.seller);
+	}
+
+	// Called by the buyer
+	function cancelPurchase(uint txId) public {
+		require(msg.sender == transactions[txId].buyer, "Not the buyer");
+		console.log("Transaction canceled: '%s'", transactions[txId].item);
+		completeTransaction(txId, Status.CANCELED);
+
+		Transaction memory transaction = transactions[txId];
+		(bool success, ) = transaction.buyer.call{ value: transaction.price }("");
+		require(success, "Failed to send Ether");
+		console.log("Payment refunded to buyer %s", transaction.buyer);
+	}
+
+	// returns the transactionId
+	function beginTransaction(address buyer, address seller, string memory item, uint price) private returns (Transaction memory) {
+		Transaction memory newTransaction = Transaction(++lastTransactionId, buyer, seller, item, price, Status.PURCHASED);
+		transactions[newTransaction.id] = newTransaction;
+		++activeTransactions;
+		return newTransaction;
+	}
+
+	function completeTransaction(uint txId, Status finalStatus) private {
+		transactions[txId].status = finalStatus;
+		--activeTransactions;
+		++completedTransactions;
+		if (Status.RECEIVED == finalStatus) {
+			emit SaleCompleted(txId, transactions[txId].buyer, transactions[txId].seller, transactions[txId].item, transactions[txId].price);
+		}
+	}
+
+	function checkTransactionStatus(uint txId) public view returns (string memory) {
+		Transaction memory transaction = transactions[txId];
+		console.log("Item '%s': status = %s", transaction.item, statusToString(transaction.status));
+		return statusToString(transaction.status);
+	}
+
+//	function checkMyBalance(address _address) public view returns (uint) {
+//		return _address.balance;
+//	}
+
+	function numActiveTransactions() public view returns (uint) {
+		return activeTransactions;
+	}
+
+	function numCompletedTransactions() public view returns (uint) {
+		return completedTransactions;
+	}
+
+	function totalInEscrow() public view returns (uint) {
+		return address(this).balance;
+	}
+
+	function lastTransaction() public view returns (Transaction memory) {
+		return transactions[lastTransactionId];
+	}
+
+	function statusToString(Status status) private pure returns (string memory) {
+		if (status == Status.PURCHASED) {
+			return "PURCHASED";
+		} else if (status == Status.SHIPPED) {
+			return "SHIPPED";
+		} else if (status == Status.RECEIVED) {
+			return "RECEIVED";
+		} else {
+			return "UNKNOWN";
+		}
+	}
+
+	constructor(address _developer) {
+		developer = _developer;
+	}
+
+	receive() external payable {}
+	fallback() external payable {}
+}
